558f5b2ee38a8d3550e979356b0fee815b4f755e
KyunghyunKimRRC <121976813+KyunghyunKimAceProject@users.noreply.github.com>
2023-10-19 13:15:50 -0500

    data and seed initial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'csv'
+
+csv_file_path = Rails.root.join('db', 'data.csv')
+csv = CSV.read(csv_file_path, headers: true)
+
+print(csv[0]['product_category_tree'].split('>>')[0].tr('["%', '').strip())
