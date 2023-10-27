7291152f13700bf40637e661cd0627743bba44de
eseo <edwardhwseo@gmail.com>
2023-10-27 10:19:11 -0500

    Linked nav bar items

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,4 +7,8 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @products = @category.products.order(:id).page params[:page]
   end
+
+  # def deal
+  #   @deals = Product.where(:discounted_price < :retail_price)
+  # end
 end
