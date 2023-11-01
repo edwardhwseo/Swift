d5b8a8ece711c26736619a0413ad5bf0641ef7b0
KyunghyunKimRRC <kkim60@academic.rrc.ca>
2023-11-01 14:02:20 -0500

    Update pages_controller.rb
    Commented unnecessary page controls

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,52 +12,52 @@ class PagesController < ApplicationController
     @products = Product.where('sale_price < price').order(:id).page params[:page]
   end
 
-  # GET /pages/new
-  def new
-    @page = Page.new
-  end
+  # # GET /pages/new
+  # def new
+  #   @page = Page.new
+  # end
 
-  # GET /pages/1/edit
-  def edit
-  end
+  # # GET /pages/1/edit
+  # def edit
+  # end
 
   # POST /pages or /pages.json
-  def create
-    @page = Page.new(page_params)
+  # def create
+  #   @page = Page.new(page_params)
 
-    respond_to do |format|
-      if @page.save
-        format.html { redirect_to page_url(@page), notice: "Page was successfully created." }
-        format.json { render :show, status: :created, location: @page }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @page.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @page.save
+  #       format.html { redirect_to page_url(@page), notice: "Page was successfully created." }
+  #       format.json { render :show, status: :created, location: @page }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @page.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /pages/1 or /pages/1.json
-  def update
-    respond_to do |format|
-      if @page.update(page_params)
-        format.html { redirect_to page_url(@page), notice: "Page was successfully updated." }
-        format.json { render :show, status: :ok, location: @page }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @page.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @page.update(page_params)
+  #       format.html { redirect_to page_url(@page), notice: "Page was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @page }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @page.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /pages/1 or /pages/1.json
-  def destroy
-    @page.destroy
+  # def destroy
+  #   @page.destroy
 
-    respond_to do |format|
-      format.html { redirect_to pages_url, notice: "Page was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to pages_url, notice: "Page was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
