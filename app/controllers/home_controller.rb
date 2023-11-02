22c257441758a13bc1cc9d93e08fd22732329dab
KyunghyunKimRRC <kkim60@academic.rrc.ca>
2023-11-02 12:50:30 -0500

    Update home_controller.rb
    Changed pagination for home page to show only 10 at a time.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.select("*, ((sale_price - price) / price * 100) AS price_difference").order("price_difference DESC").page params[:page]
+    @products = Product.select("*, ((price - sale_price) / price * 100) AS price_difference").order("price_difference DESC").page params[:page]
+    @products = @products.per(10) if request.path == root_path
   end
 end
