7c64eabdcd7c00dfdf75b28ea5a2ee674d21d944
eseo <edwardhwseo@gmail.com>
2023-11-09 11:14:33 -0600

    Update Cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,21 +1,24 @@
 class CartsController < ApplicationController
-    #POST /cart
+    #POST /carts
     def create
         # log product id to the terminal logger
-        logger.debug("Adding #{params[:id]} to cart")
+        #logger.debug("Adding #{params[:id]} to cart")
         id = params[:id].to_i
         session[:shopping_cart] << id unless session[:shopping_cart].include?(id) # Pushes id onto the end of array
         product = Product.find(id)
-        flash[:notice] = "#{product.name} added to cart"
-        # redirect_to root_path
+        flash[:notice] = "*#{product.name} added to cart"
+        session[:return_to] = product_path(product)
+        redirect_to session[:return_to]
     end
 
-    # DELETE /cart/:id
+    # DELETE /carts/:id
     def destroy
-        logger.debug("removing #{params[:id]} from cart.")
+        #logger.debug("removing #{params[:id]} from cart.")
         id = params[:id].to_i
         session[:shopping_cart].delete(id)
-        # TODO - add notification...
-        redirect_to root_path
+        product = Product.find(id)
+        flash[:notice] = "*#{product.name} removed from cart"
+        session[:return] = product_path(product)
+        redirect_to session[:return]
     end
 end
