8b9ff3bfe7963b6b5655f3e822f039cb522c35d2
KyunghyunKimRRC <kkim60@academic.rrc.ca>
2023-11-21 10:45:03 -0600

    Login and Logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,32 @@ class ApplicationController < ActionController::Base
         @categories = Category.where(name: category_names)
     end
 
+    def login
+        user = User.find_by(email: params[:email])
+        # if user && user.authenticate(params[:password])
+        #     redirect_to root_path, notice: 'Logged in successfully!'
+        # else
+        #     flash.now[:alert] = 'Invalid email or password'
+        #     redirect_to root_path, notice: 'Login failed'
+        # end
+        session[:user] = user.email
+        redirect_to root_path, notice: 'Logged in successfully!'
+    end
+
+    def logout
+        session[:user] = nil
+        redirect_to root_path, notice: 'Logged out successfully!'
+    end
+
+    def register
+        user = User.new(user_params)
+        if user.save
+            redirect_to root_path, notice: 'Registered successfully!'
+        else
+            flash.now[:alert] = 'Error registering user'
+        end
+    end
+
     private
 
     def initialize_session
@@ -21,5 +47,9 @@ class ApplicationController < ActionController::Base
         else
             []
         end
+    end
+
+    def user_params
+        params.require(:user).permit(:email, :password, :first_name, :last_name)
     end
 end
